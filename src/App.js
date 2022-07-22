9f5f9892c5533db38fcb0d88a0ee04b1b8111e7f
Joey-Flores <joeyflores98@gmail.com>
2022-07-22 14:24:48 -0700

    finished login auth, and logout. created acc page

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import classes from "./App.module.css";
 import GameSection from "./components/GameSection/GameSection";
 import RulesSection from "./components/RulesSection/RulesSection";
 import TitleSection from "./components/TitleSection/TitleSection";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   const [score, setScore] = useState(0);
   const [gameStatus, setGameStatus] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
+  const [userData, setUserData] = useState({});
 
   function helpScore() {
     if (gameStatus === "WIN") {
@@ -16,15 +19,29 @@ function App() {
     }
   }
 
+  function handleLoggedInStatus() {
+    setIsLoggedIn((current) => !current);
+  }
+
   return (
     <div className={classes.App}>
       <TitleSection score={score} />
-      <GameSection
-        helpScore={helpScore}
-        gameStatus={gameStatus}
-        setGameStatus={setGameStatus}
-      />
-      <RulesSection />
+      <Link to="/start">
+        <button className={classes.startButton}>START PLAYING</button>
+      </Link>
+      <Routes>
+        <Route
+          path="start"
+          element={
+            <GameSection
+              helpScore={helpScore}
+              gameStatus={gameStatus}
+              setGameStatus={setGameStatus}
+            />
+          }
+        />
+      </Routes>
+      <RulesSection logStatus={isLoggedIn} isLoggedIn={handleLoggedInStatus} />
     </div>
   );
 }
