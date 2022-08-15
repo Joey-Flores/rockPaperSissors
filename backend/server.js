f479cf43e113a5ce02ff316265973ff87135f93c
Joey-Flores <joeyflores98@gmail.com>
2022-08-15 16:04:52 -0700

    test

--- backend/server.js
+++ backend/server.js
@@ -168,7 +168,7 @@ app.get("/account", (req, res) => {
 if (process.NODE_ENV == "production") {
   app.use(express.static("client/build"));
   const path = require("path");
-  app.get("*", (req, res) => {
+  app.get("/**", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
