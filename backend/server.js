fce8ccca8480109b3bd8b03d986af9437e451062
Joey-Flores <joeyflores98@gmail.com>
2022-08-15 09:15:56 -0700

    test server upload

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,17 @@ const app = express();
 
 const User = require("./models/user");
 
+if (process.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 //Mongodb
 const mongoose = require("mongoose");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 const dbUrl =
   process.env.DB_USER ||
@@ -159,6 +167,11 @@ app.post("/score", async (req, res) => {
 app.get("/account", (req, res) => {
   res.send(req.user);
 });
+
+// app.use(express.static(path.join(__dirname, "../build")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../build"));
+// });
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
