5f2cbed1e2291ba4c71f1d34a951ce80d152b3c9
rameezamem1 <rameeza.memon1@gmail.com>
2022-08-17 00:59:00 +0500

    project directory changed

--- backend/server.js
+++ backend/server.js
@@ -167,13 +167,11 @@ app.get("/account", (req, res) => {
 //     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 //   });
 // }
-console.log(process.env.NODE_ENV)
-if (process.env.NODE_ENV === 'production') {
+console.log(__dirname + '/client/build/index.html')
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build / index.html'));
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
   });
-}
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
